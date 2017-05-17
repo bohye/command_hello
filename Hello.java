c00b5f8ff75573d9e436fa4d3458ac9b6c138de9
최보혜 <bohye.choi@navercorp.com>
2017-05-17 16:05:00 +0900

    bad annotation delete

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 // For command line git tutorial
 // First : GitHub modification
 // Second : Local repository modification
->>>>>>> origin/master
 public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello world");
